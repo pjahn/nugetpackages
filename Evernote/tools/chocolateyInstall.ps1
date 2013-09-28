8afd98286b7e7530a7d60512855c2f9831412c65
Peter Jahn <peterjahn@gmail.com>
2013-09-28 16:45:25 +0200

    Add Evernote

---
+++ Evernote/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'Evernote'
+$url = 'http://evernote.com/download/get.php?file=Win'
+$url64 = $url
+$fileType = 'exe'
+$silentArgs = '/quiet'
+
+try {
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
