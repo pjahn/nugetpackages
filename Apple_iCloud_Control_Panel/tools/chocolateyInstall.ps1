4e1e544764b335e5a7c1daa36e6d9dbf1d5270d6
Peter Jahn <peterjahn@gmail.com>
2013-09-28 15:34:58 +0200

    Add Apple iCloud Conrol Panel 3.0.2.163

---
+++ Apple_iCloud_Control_Panel/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$packageName = 'Apple_Airport_Utility'
+$url = 'http://support.apple.com/downloads/DL1455/en_US/iCloudSetup.exe'
+$url64 = $url
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$filePath = "$env:TEMP\chocolatey\$packageName"
+$fileFullPath = "$filePath\$packageName_Install.exe"
+
+try {
+    if (-not (Test-Path $filePath)) {
+        New-Item -ItemType directory -Path $filePath
+    }
+
+    Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
+
+    Start-Process "7za" -ArgumentList "x -o`"$filePath`" -y `"$fileFullPath`"" -Wait
+
+    $processor = Get-WmiObject Win32_Processor
+    $is64bit = $processor.AddressWidth -eq 64
+
+    $packageName = 'bonjour'
+    if ($is64bit) {$file = "$filePath\Bonjour64.msi"} else {$file = "$filePath\Bonjour.msi"}
+    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file	
+
+    $packageName = 'icloud'
+    if ($is64bit) {$file = "$filePath\iCloud64.msi"} else {$file = "$filePath\Bonjour.msi"}
+    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file	
+
+	
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
