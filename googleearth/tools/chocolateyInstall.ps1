edb16f5c86a1b8c10e1f38f6c4da6ec817ac8471
Peter Jahn <peterjahn@gmail.com>
2013-10-04 20:30:04 +0200

    add google earh

---
+++ googleearth/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$packageName = 'GoogleEarth'
+$url = 'https://dl.google.com/earth/client/advanced/current/GoogleEarthWin.exe'
+$url64 = $url
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$filePath = "$env:TEMP\chocolatey\$packageName"
+$fileFullPath = "$filePath\$packageName_Install.exe"
+
+try {
+    if (-not (Test-Path $filePath)) {
+        New-Item -ItemType directory -Path $filePath
+    }
+
+    Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
+
+    Start-Process "7za" -ArgumentList "x -o`"$filePath`" -y `"$fileFullPath`"" -Wait
+
+    $packageName = 'googleearth'
+    $file = "$filePath\Google Earth.msi"
+    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file	
+	
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
