73785be7669b32e5a3c626fe0ef0e42d142777e2
Peter Jahn <peterjahn@gmail.com>
2013-10-04 23:33:21 +0200

    add microsoft sms sender

---
+++ Microsoft_SMS_Sender/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'Microsoft_SMS_Sender' # arbitrary name for the package, used in messages
+$installerType = 'MSI' #only one of these two: exe or msi
+$url = 'http://download.microsoft.com/download/8/f/d/8fd4e1cd-b2d7-4e23-9c5b-54b76fa222b9/smssender.msi' # download url
+$url64 = $url
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
