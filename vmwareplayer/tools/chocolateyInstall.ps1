f14802f90148247f35ac878a93b01fdb42e5855f
Peter Jahn <peterjahn@gmail.com>
2013-10-05 22:59:25 +0200

    add vmware player

---
+++ vmwareplayer/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'vmwareplayer'
+$installerType = 'EXE'
+$url = 'https://download3.vmware.com/software/player/file/VMware-player-6.0.0-1295980.exe'
+$url64 = $url
+$silentArgs = '/s /nsr /v EULAS_AGREED=1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
