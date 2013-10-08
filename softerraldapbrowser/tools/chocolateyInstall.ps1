a5b3822536719f5c1a370f6841b9880a79d23006
Peter Jahn <peterjahn@gmail.com>
2013-10-08 20:27:07 +0200

    add softerra ldap browser

---
+++ softerraldapbrowser/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'softerraldapbrowser'
+$installerType = 'MSI'
+$url = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.10625.0-x86-eng.msi'
+$url64 = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.10625.0-x64-eng.msi'
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
