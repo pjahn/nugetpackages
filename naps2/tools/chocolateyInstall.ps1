868a22f7763d977a1e02b4db364e4e77759eed56
Peter Jahn <peterjahn@gmail.com>
2013-10-10 23:21:55 +0200

    add naps2

---
+++ naps2/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿
+
+$packageName = 'naps2' 
+$installerType = 'MSI' 
+$url = 'http://optimate.dl.sourceforge.net/project/naps2/2.6.2/naps2-2.6.2-setup.msi' 
+$url64 = $url 
+$silentArgs = '/quiet /norestart' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
