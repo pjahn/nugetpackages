0fa0faa2a40794b8f9e7b90e7bffe1e8f0a8a543
Peter Jahn <peterjahn@gmail.com>
2013-10-30 23:59:31 +0100

    add IrfanView Plugins

---
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'irfanviewplugins'
+$installerType = 'EXE'
+$url = 'http://www12.tucows.com/windows/files/irfanview_plugins_436_setup.exe'
+$url64 = $url
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
