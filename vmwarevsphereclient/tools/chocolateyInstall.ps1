940054b7f5970b3dbf467fde95eb1fc626ed4985
Peter Jahn <peterjahn@gmail.com>
2013-11-01 11:53:19 +0100

    add VMware Vsphere Client

---
+++ vmwarevsphereclient/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'vmwarevsphereclient' 
+$installerType = 'exe' 
+$url = 'http://vsphereclient.vmware.com/vsphereclient/1/2/8/1/6/5/0/VMware-viclient-all-5.5.0-1281650.exe' 
+$url64 = $url 
+$silentArgs = '/s /w /v" /qb"' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
