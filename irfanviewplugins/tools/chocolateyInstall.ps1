eb4b75d960ff4e310d6550384eb9fd864aa26ee7
Peter Jahn <peterjahn@gmail.com>
2013-12-29 22:41:07 +0100

    update IrvanView Plugins to version 4.3.7.0

--- irfanviewplugins/tools/chocolateyInstall.ps1
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'irfanviewplugins'
 $installerType = 'EXE'
-$url = 'http://www12.tucows.com/windows/files/irfanview_plugins_436_setup.exe'
+$url = 'http://www12.tucows.com/windows/files/irfanview_plugins_437_setup.exe'
 $url64 = $url
 $silentArgs = '/silent'
 $validExitCodes = @(0)
