699041ea023c22a6a2557f96eefed0299c3d8eae
Peter Jahn <peterjahn@gmail.com>
2014-02-15 21:46:08 +0100

    Update NAPS2 to version 2.6.3

--- naps2/tools/chocolateyInstall.ps1
+++ naps2/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'naps2' 
 $installerType = 'MSI' 
-$url = 'http://optimate.dl.sourceforge.net/project/naps2/2.6.2/naps2-2.6.2-setup.msi' 
+$url = 'http://garr.dl.sourceforge.net/project/naps2/2.6.3/naps2-2.6.3-setup.msi' 
 $url64 = $url 
 $silentArgs = '/quiet /norestart' 
 $validExitCodes = @(0) 
