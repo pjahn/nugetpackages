af562a27383e99c97c950932be70cb57b83b6f07
Peter Jahn <peterjahn@gmail.com>
2014-07-11 22:20:44 +0200

    Update Evernote and VMware Player

--- vmwareplayer/tools/chocolateyInstall.ps1
+++ vmwareplayer/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'vmwareplayer'
 $installerType = 'EXE'
-$url = 'https://download3.vmware.com/software/player/file/VMware-player-6.0.1-1379776.exe'
+$url = 'https://download3.vmware.com/software/player/file/VMware-player-6.0.3-1895310.exe'
 $url64 = $url
 $silentArgs = '/s /nsr /v EULAS_AGREED=1'
 $validExitCodes = @(0)
