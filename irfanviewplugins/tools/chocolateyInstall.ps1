bbfee139d043f5750fc605056fc34ac5d4bd751f
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 12:14:46 -0500

    Updated irfanview plugins to 4.38
    changed the versionning to match official versionning (
    http://www.irfanview.info/main_history.htm ) and irfanview package (
    https://chocolatey.org/packages/IrfanView )

--- irfanviewplugins/tools/chocolateyInstall.ps1
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'irfanviewplugins'
 $installerType = 'EXE'
-$url = 'http://www12.tucows.com/windows/files/irfanview_plugins_437_setup.exe'
+$url = 'http://www.tucows.com/download/windows/files/irfanview_plugins_438_setup.exe'
 $url64 = $url
 $silentArgs = '/silent'
 $validExitCodes = @(0)
