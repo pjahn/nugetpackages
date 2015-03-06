e1d4d7b7f2696ac8177f5571933168299c39dd3d
Peter Jahn <peterjahn@gmail.com>
2015-03-06 20:50:06 +0100

    Update VMware vSphere Client to version 5.5.0 - 1993072

--- vmwarevsphereclient/tools/chocolateyInstall.ps1
+++ vmwarevsphereclient/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'vmwarevsphereclient' 
 $installerType = 'exe' 
-$url = 'http://vsphereclient.vmware.com/vsphereclient/1/2/8/1/6/5/0/VMware-viclient-all-5.5.0-1281650.exe' 
+$url = 'http://vsphereclient.vmware.com/vsphereclient/1/9/9/3/0/7/2/VMware-viclient-all-5.5.0-1993072.exe'
 $url64 = $url 
 $silentArgs = '/s /w /v" /qb"' 
 $validExitCodes = @(0) 
