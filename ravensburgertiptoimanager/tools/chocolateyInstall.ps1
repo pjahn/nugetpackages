8c393377b87a51a91a4d65de6b6b3e704b15a33b
Peter Jahn <peterjahn@gmail.com>
2015-03-20 23:25:37 +0100

    Ravensburger tiptoi Manager

---
+++ ravensburgertiptoimanager/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'ravensburgertiptoimanager' 
+$fileType = 'exe'
+$url = 'http://static.tiptoi.com/software/windows/install.exe' 
+$url64 = $url 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+try {
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
