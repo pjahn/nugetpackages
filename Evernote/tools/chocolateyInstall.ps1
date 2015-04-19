6eeaf52860a5b17000d6d7b2c01d35d8ffe44694
Peter Jahn <peterjahn@gmail.com>
2015-04-19 12:36:04 +0200

    Simpler install script for evernote

--- Evernote/tools/chocolateyInstall.ps1
+++ Evernote/tools/chocolateyInstall.ps1
@@ -4,12 +4,4 @@ $url64 = $url
 $fileType = 'exe'
 $silentArgs = '/quiet'
 
-try {
-
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
-
-    Write-ChocolateySuccess "$packageName"
-} catch {
-    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-    throw
-}
