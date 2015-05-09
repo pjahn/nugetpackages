f150ee3ab70f88ac6431247958271594f6896825
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-09 16:08:58 +0200

    update vSphere Client 6.0.0

--- vmwarevsphereclient/tools/chocolateyInstall.ps1
+++ vmwarevsphereclient/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'vmwarevsphereclient' 
-$installerType = 'exe' 
-$url = 'http://vsphereclient.vmware.com/vsphereclient/1/9/9/3/0/7/2/VMware-viclient-all-5.5.0-1993072.exe'
-$url64 = $url 
-$silentArgs = '/s /w /v" /qb"' 
-$validExitCodes = @(0) 
+$installerType = 'exe'
+$url = 'http://vsphereclient.vmware.com/vsphereclient/VMware-viclient-all-6.0.0.exe'
+$url64 = $url
+$silentArgs = '/s /w /v" /qb"'
+$validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
