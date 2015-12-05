72381784fdcf3e4863f37b8ca4f2c4ebb7e445dd
Peter Jahn <peterjahn@gmail.com>
2015-12-05 15:39:30 +0100

    Update Softerra LDAP Browser to version 4.5.13724.0

--- softerraldapbrowser/tools/chocolateyInstall.ps1
+++ softerraldapbrowser/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'softerraldapbrowser'
 $installerType = 'MSI'
-$url = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.10625.0-x86-eng.msi'
-$url64 = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.10625.0-x64-eng.msi'
+$url = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.13724.0-x86-eng.msi'
+$url64 = 'http://softerra-downloads.com/ldapadmin/ldapbrowser-4.5.13724.0-x64-eng.msi'
 $silentArgs = '/quiet /norestart'
 $validExitCodes = @(0)
 
