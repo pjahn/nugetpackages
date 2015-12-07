69edc181871061daeb4e95f5db9e1be626ebda3b
Peter Jahn <peterjahn@gmail.com>
2015-12-07 23:52:27 +0100

    Update to Irfan View Plugins to version 4.4.1

--- irfanviewplugins/tools/chocolateyInstall.ps1
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -1,8 +1,20 @@
-﻿$packageName = 'irfanviewplugins'
-$installerType = 'EXE'
-$url = 'http://www.tucows.com/download/windows/files/irfanview_plugins_438_setup.exe'
-$url64 = $url
-$silentArgs = '/silent'
+$packageName = 'irfanviewplugins'
+$installerType = 'exe'
+$silentArgs = "/silent"
+$urlArray = @('http://www.fosshub.com/genLink/IrfanView/irfanview_plugins_441_setup.exe','http://www.fosshub.com/genLink/IrfanView/irfanview_plugins_x64_441_setup.exe')
+$url = $urlArray[0]
+$url64 = $urlArray[1]
 $validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if (!(Get-Command Get-UrlFromFosshub -ErrorAction SilentlyContinue)) {
+  Import-Module "$($toolsDir)\Get-UrlFromFosshub.ps1"
+}
+$url = Get-UrlFromFosshub $url
+$url64 = Get-UrlFromFosshub $url64
+Write-Host $url64
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes
