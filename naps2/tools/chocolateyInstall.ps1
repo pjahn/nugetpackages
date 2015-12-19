2286d1153e09896fcf875fb9ae6a87e3f343daf3
Peter Jahn <peterjahn@gmail.com>
2015-12-19 19:56:35 +0100

    Update naps2 to version 4.3.1

--- naps2/tools/chocolateyInstall.ps1
+++ naps2/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'naps2' 
 $installerType = 'MSI' 
-$url = 'https://github.com/cyanfish/naps2/releases/download/v4.2.3/naps2-4.2.3-setup.msi' 
+$url = 'https://github.com/cyanfish/naps2/releases/download/v4.3.1/naps2-4.3.1-setup.msi' 
 $url64 = $url 
 $silentArgs = '/quiet /norestart' 
 $validExitCodes = @(0) 
