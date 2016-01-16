7cbb575ada1dec8d02267bceeb2111fe708b11cb
Peter Jahn <peterjahn@gmail.com>
2016-01-16 23:49:09 +0100

    Update NAPS2 to version 4.5.1

--- naps2/tools/chocolateyInstall.ps1
+++ naps2/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'naps2' 
 $installerType = 'MSI' 
-$url = 'https://github.com/cyanfish/naps2/releases/download/v4.5.0/naps2-4.5.0-setup.msi' 
+$url = 'https://github.com/cyanfish/naps2/releases/download/v4.5.1/naps2-4.5.1-setup.msi' 
 $url64 = $url 
 $silentArgs = '/quiet /norestart' 
 $validExitCodes = @(0) 
