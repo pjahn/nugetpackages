cda051a15d487993c5427d4674b721041e699e8c
aseques <aseques@gmail.com>
2017-03-06 09:54:26 +0100

    Updated link to current version (6.0 Update 3)
    Version in the reposotory is Client 6.0 Update 2, the modified link is for the current version

--- vmwarevsphereclient/tools/chocolateyInstall.ps1
+++ vmwarevsphereclient/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'vmwarevsphereclient' 
 $installerType = 'exe' 
-$url = 'http://vsphereclient.vmware.com/vsphereclient/2/5/0/2/2/2/2/VMware-viclient-all-6.0.0-2502222.exe'
+$url = 'http://vsphereclient.vmware.com/vsphereclient/5/1/1/2/5/0/8/VMware-viclient-all-6.0.0-5112508.exe'
 $url64 = $url 
 $silentArgs = '/s /w /v" /qb"' 
 $validExitCodes = @(0) 
